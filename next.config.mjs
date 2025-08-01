035d47ceb9764a4e1bffd60f3dedb08656334cd1
Anurag <144671360+ANURAGGGGGGGGGGGG@users.noreply.github.com>
2025-08-01 16:37:46 +0530

    Zesty Bite

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,  // ✅ disables ESLint for next build & deploy
   },
+  images: {
+    domains: [
+      "b.zmtcdn.com",
+      "static.toiimg.com",
+      "media-assets.swiggy.com",
+      "s7d1.scene7.com",
+      "www.inspiredtaste.net",
+      "ichef.bbci.co.uk",
+      "images-prod.healthline.com",
+      "cdn.loveandlemons.com"
+    ]
+  }
 };
 
 export default nextConfig;
